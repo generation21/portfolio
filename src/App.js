542d046d6cb7ae9a3002728e62c0a38b4c21fb9c
generation21 <qbration21@gmail.com>
2023-07-04 23:12:06 +0900

    z-index 문제로 가장 상위 컴포넌트로 dialog 옮김

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import styles from "src/styles/App.module.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Information from "./components/Information";
 import NavigationBar from "./components/NavigationBar";
+import NewChat from "./components/NewChat";
 
 export const HOME_PATH = "/home";
 export const ABOUT_PATH = "/about";
@@ -10,9 +13,33 @@ export const PROJECTS_PATH = "/projects";
 export const SKILLS_PATH = "/skills";
 
 function App() {
+    const [isNewChatOpen, setIsNewChatOpen] = useState(false);
+    const [isInforOpen, setIsInforOpen] = useState(false);
+    const handleOnClickIsOpen = () => {
+        setIsNewChatOpen((prev) => !prev);
+    };
+    const handleOnClickInfor = () => {
+        setIsInforOpen((prev) => !prev);
+    };
     return (
         <div className={styles.container}>
-            <NavigationBar />
+            {isNewChatOpen && (
+                <NewChat
+                    key={"newChat"}
+                    handleOnClickIsOpen={handleOnClickIsOpen}
+                />
+            )}
+            {isInforOpen && (
+                <Information
+                    key={"Contact"}
+                    handleOnClickIsOpen={handleOnClickInfor}
+                />
+            )}
+
+            <NavigationBar
+                handleOnClickIsOpen={handleOnClickIsOpen}
+                handleOnClickInfor={handleOnClickInfor}
+            />
             <main className={styles.main}>
                 <Header />
                 <Outlet />
