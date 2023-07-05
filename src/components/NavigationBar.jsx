89f76c43ba7936ef7d1ed7a28be1f6785f2d015e
generation21 <qbration21@gmail.com>
2023-07-06 00:57:28 +0900

    hidebar fixed

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import { ABOUT_PATH, HOME_PATH, PROJECTS_PATH, SKILLS_PATH } from "src/App";
+import { ABOUT_PATH, HOME_PATH, PROJECTS_PATH, PAPERS_PATH } from "src/App";
 import { BsLinkedin, BsGithub } from "react-icons/bs";
 import { BiMessage } from "react-icons/bi";
 import { FaPlus } from "react-icons/fa";
@@ -14,6 +14,7 @@ export default function NavigationBar({
     handleOnClickIsOpen,
     handleOnClickInfor,
 }) {
+    const [isSideBar, setIsSideBar] = useState(false);
     const { pathname } = useLocation();
     const portfolios = [
         {
@@ -33,83 +34,114 @@ export default function NavigationBar({
         },
         {
             title: "Tell me about SB's Papers",
-            path: SKILLS_PATH,
+            path: PAPERS_PATH,
             icon: <BiMessage className={styles.icon} />,
         },
     ];
 
-    return (
-        <nav className={styles.navigation}>
-            <div>
-                <div className={styles.menue}>
-                    <button
-                        className={styles.newchat}
-                        onClick={handleOnClickIsOpen}
-                    >
-                        <FaPlus />
-                        New chat
-                    </button>
-                    <button className={styles.hide_slider}>
-                        <RiBookletFill className={styles.icon} />
-                    </button>
-                </div>
-                {portfolios.map((project, index) => (
-                    <Link to={project.path}>
-                        <div
-                            key={index}
-                            className={`${styles.container} ${
-                                pathname === project.path && styles.active
-                            }  `}
-                        >
-                            <span
-                                className={`${styles.icon_contain} ${
-                                    pathname === project.path && styles.active
-                                }  `}
-                            >
-                                {project.icon}
-                            </span>
-                            <p
-                                className={`${styles.title} ${
-                                    pathname === project.path && styles.active
-                                }  `}
-                            >
-                                {project.title}
-                            </p>
-                        </div>
-                    </Link>
-                ))}
-            </div>
-            <div className={styles.about}>
-                <button className={styles.button} onClick={handleOnClickInfor}>
-                    <img
-                        src="/image/favicon.ico"
-                        alt="profile"
-                        width={40}
-                        height={40}
-                    />
-                    <p className={styles.title}>홍승범</p>
-                </button>
+    const handleOnClickSide = () => {
+        setIsSideBar((prev) => !prev);
+    };
 
-                <div className={styles.items}>
-                    <Link
-                        to={
-                            "https://www.linkedin.com/in/seungbum-hong-531311171/"
-                        }
-                        target="_blank"
-                    >
-                        <BsLinkedin className={styles.icon} />
-                    </Link>
-                    <Link
-                        to={"https://github.com/generation21"}
-                        target="_blank"
-                    >
-                        <BsGithub className={styles.icon} />
-                    </Link>
-                    <Link>
-                        <SiTistory className={styles.icon} />
-                    </Link>
-                </div>
+    if (isSideBar) {
+        return (
+            <div className={styles.hide__container}>
+                <button
+                    aria-label={"Show sidebar"}
+                    className={styles.hide_slider}
+                    onClick={handleOnClickSide}
+                >
+                    <RiBookletFill className={styles.icon} />
+                    <span className={styles.tooltip} style={{ left: "4rem" }}>
+                        Show sidebar
+                    </span>
+                </button>
             </div>
-        </nav>
-    );
+        );
+    } else {
+        return (
+            <nav className={`${styles.navigation} ${isSideBar && styles.hide}`}>
+                <div>
+                    <div className={styles.menue}>
+                        <button
+                            className={styles.newchat}
+                            onClick={handleOnClickIsOpen}
+                        >
+                            <FaPlus />
+                            New chat
+                        </button>
+                        <button
+                            aria-label="Show sidebar"
+                            className={styles.hide_slider}
+                            onClick={handleOnClickSide}
+                        >
+                            <RiBookletFill className={styles.icon} />
+                            <span className={styles.tooltip}>Hide sidebar</span>
+                        </button>
+                    </div>
+                    {portfolios.map((project, index) => (
+                        <Link to={project.path}>
+                            <div
+                                key={index}
+                                className={`${styles.container} ${
+                                    pathname === project.path && styles.active
+                                }  `}
+                            >
+                                <span
+                                    className={`${styles.icon_contain} ${
+                                        pathname === project.path &&
+                                        styles.active
+                                    }  `}
+                                >
+                                    {project.icon}
+                                </span>
+                                <p
+                                    className={`${styles.title} ${
+                                        pathname === project.path &&
+                                        styles.active
+                                    }  `}
+                                >
+                                    {project.title}
+                                </p>
+                            </div>
+                        </Link>
+                    ))}
+                </div>
+                <div className={styles.about}>
+                    <button
+                        className={styles.button}
+                        onClick={handleOnClickInfor}
+                    >
+                        <img
+                            src="/image/favicon.ico"
+                            alt="profile"
+                            width={40}
+                            height={40}
+                        />
+                        <p className={styles.title}>홍승범</p>
+                    </button>
+
+                    <div className={styles.items}>
+                        <Link
+                            to={
+                                "https://www.linkedin.com/in/seungbum-hong-531311171/"
+                            }
+                            target="_blank"
+                        >
+                            <BsLinkedin className={styles.icon} />
+                        </Link>
+                        <Link
+                            to={"https://github.com/generation21"}
+                            target="_blank"
+                        >
+                            <BsGithub className={styles.icon} />
+                        </Link>
+                        <Link>
+                            <SiTistory className={styles.icon} />
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        );
+    }
 }
